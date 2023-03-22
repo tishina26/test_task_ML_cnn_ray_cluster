feb9795ad0f1e2906f735770e360a945a9b20264
tishina26 <s02210569@gse.cs.msu.ru>
2023-03-22 17:32:54 +0300

    cnn with ray

--- ray_light_cnn.py
+++ ray_light_cnn.py
@@ -5,6 +5,7 @@ import ray
 from ray import air, tune
 from ray.tune.integration.keras import TuneReportCallback
 
+# importing the required packages
 import tensorflow as tf
 # импорт слоев
 from tensorflow.keras import layers
@@ -18,24 +19,31 @@ from pathlib import Path
 def train_mnist(config):
     num_classes = 10
 
+    # loading the MNIST dataset
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
-    x_train, x_test = x_train / 255.0, x_test / 255.0
+    # reshaping the training and testing data
+    x_train = x_train.reshape((x_train.shape[0], x_train.shape[1], x_train.shape[2], 1))
+    x_test = x_test.reshape((x_test.shape[0], x_test.shape[1], x_test.shape[2], 1))
 
-    y_train = tf.keras.utils.to_categorical(y_train, num_classes)
-    y_test = tf.keras.utils.to_categorical(y_test, num_classes)
+    # normalizing the values of pixels of images
+    x_train = x_train.astype('float32') / 255.0
+    x_test = x_test.astype('float32') / 255.0
 
-    if (Path("./model_rnn.h5").exists()):
-        model = tf.keras.models.load_model('./model_rnn.h5')
+
+    if (Path("./model_cnn.h5").exists()):
+        model = tf.keras.models.load_model('./model_cnn.h5')
     else:
         model = Sequential([
-            layers.Dense(config["neiron_0"], activation=config["activation_0"], input_shape=(x_train[0].shape)),
-            layers.Dense(config["neiron_1"], activation=config["activation_1"], input_shape=(x_train[0].shape)),
+            layers.Conv2D(config["neiron_0"], (3, 3), activation=config["activation_0"], input_shape=(x_train[0].shape)),
+            layers.MaxPool2D((2, 2)),
+            layers.Dropout(0.5),
             layers.Flatten(),
+            layers.Dense(config["neiron_1"], activation=config["activation_1"]),
             layers.Dense(10, activation='softmax')
         ])
 
-    model.compile(optimizer=config["optimizer"], loss='categorical_crossentropy', metrics=['accuracy'])
-    checkpoint = tf.keras.callbacks.ModelCheckpoint("./model_rnn.h5", save_best_only=True)
+    model.compile(optimizer=config["optimizer"], loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    checkpoint = tf.keras.callbacks.ModelCheckpoint("./model_cnn.h5", save_best_only=True)
     model.fit(x_train, y_train, batch_size=128, epochs=config["epochs"], verbose=1, validation_split=0.1,
               callbacks=[checkpoint, TuneReportCallback({"mean_accuracy": "accuracy"})], )
 
@@ -51,8 +59,8 @@ if __name__ == "__main__":
         "optimizer": tune.choice(["sgd", "adam"]),
         "epochs": tune.randint(5, 10),
         # "epochs": tune.randint(1, 2),
-        "neiron_0": tune.randint(32, 512),
-        "neiron_1": tune.randint(32, 64),
+        "neiron_0": tune.randint(16, 64),
+        "neiron_1": tune.randint(128, 256),
         "activation_0": tune.choice(["sigmoid", "relu"]),
         "activation_1": tune.choice(["sigmoid", "relu"]),
     }
